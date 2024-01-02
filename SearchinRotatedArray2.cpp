5db753a372ca48967c0ca3c28957c076c78e69c9
D Khirod Kumar <113640893+Khirod24@users.noreply.github.com>
2024-01-02 10:47:56 +0530

    Create SearchinRotatedArray2.cpp

---
+++ SearchinRotatedArray2.cpp
@@ -0,0 +1,33 @@
+//LEETCODE(81)
+    bool search(vector<int>& nums, int target) {
+        int n= nums.size();
+    int low= 0, high=n-1;
+        while(low<=high){
+            int mid=(high-low)/2+low;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++, high--;
+                continue;
+            }
+            //left sorted array
+            if(nums[low]<=nums[mid]){
+             if(nums[low]<=target && target<=nums[mid]){
+                 high=mid-1;
+              }
+              else{
+                  low=mid+1;
+              }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                 low=mid+1;
+              }
+              else{
+                  high=mid-1;
+              }
+            }
+        }
+        return false;
+    }
