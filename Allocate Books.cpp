0a8afcbd1902264126f0fb816b2638c3ccd272c8
D Khirod Kumar <113640893+Khirod24@users.noreply.github.com>
2024-01-04 13:13:53 +0530

    Create Allocate Books.cpp

---
+++ Allocate Books.cpp
@@ -0,0 +1,31 @@
+//CODING NINJA AND GFG
+int countStudents(vector<int>&arr,int n, int pages){
+    int students=1;
+    long long pageStudents=0;
+    for(int i=0;i<n;i++){
+        if(pageStudents+arr[i]<=pages){
+            pageStudents+=arr[i];
+        }
+        else{
+            students++;
+            pageStudents=arr[i];
+        }
+    }
+    return students;
+}
+int findPages(vector<int>& arr, int n, int m) {
+    if(m>n)return -1;
+    int low= *max_element(arr.begin(),arr.end());//max(arr)
+    int high= accumulate(arr.begin(), arr.end(),0);//sum(arr)
+    while(low<=high){
+        int mid= (high-low)/2+low;
+        int students= countStudents(arr,n,mid);
+        if(students>m){
+            low=mid+1;
+        }
+        else{
+            high= mid-1;
+        }
+    }
+    return low;
+}
